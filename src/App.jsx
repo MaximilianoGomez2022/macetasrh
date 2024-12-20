1115be3236cfcec769452b238be7a79cfd7bab91
Maximiliano <maxigomez.udaondo@gmail.com>
2024-12-20 12:14:41 -0300

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import '../public/css/estilos.css'
 import Header from './components/Header'
 import Banner from './components/Banner'
 import Productos from './components/Productos'
-import Footer from './components/footer'
+import Footer from './components/Footer'
 import Nosotros from './components/Nosotros'
 import Pastillas from './components/Pastillas'
 
