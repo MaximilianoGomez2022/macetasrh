3f43dd32d667fdf94dcd3def03bfbc321478ada8
Maximiliano <maxigomez.udaondo@gmail.com>
2024-12-23 20:00:19 -0300

    banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,9 +3,18 @@ function Banner() {
         <div className="banner">
             <div className="texto-banner">
             <h2>CREANDO VIDA<br></br> EN CADA MACETA</h2>
-            <p>Descubre nuestra amplia variedad de macetas hechas a mano, perfectas para realzar la belleza de tus plantas y decorar tus espacios. Calidad, diseño y amor por la naturaleza en cada pieza.<br></br>¡Dale vida a tu hogar con nosotros!</p>
+            <p className="p-desktop">Descubre nuestra amplia variedad de macetas hechas a mano, perfectas para realzar la belleza de tus plantas y decorar tus espacios. Calidad, diseño y amor por la naturaleza en cada pieza.<br></br>¡Dale vida a tu hogar con nosotros!</p>
+            <p className="p-mobile">Descubre nuestra amplia variedad de macetas hechas a mano, perfectas para realzar la belleza de tus plantas y decorar tus espacios</p>
             </div>
-            <img src="./banner.jpg" alt="" srcset="" />
+            <figure>                  
+            <picture >
+                <source media="(max-width: 378px)" srcSet="./banner-cel-2.jpg"/>
+                <source media="(max-width: 576px)" srcSet="./banner.jpg"/>
+                <source media="(max-width: 768px)" srcSet="./banner.jpg"/>
+                <source media="(max-width: 991px)" srcSet="./banner.jpg"/>
+                <img src="./banner.jpg" alt="banner de portada imagen de macetas"/>
+            </picture>
+          </figure>
         </div>
     );
   }
