3938a09a27634dae0abdda0783452bcc8e63b7d5
Maximiliano <maxigomez.udaondo@gmail.com>
2024-12-25 21:38:04 -0300

    modal

--- src/components/Productos.jsx
+++ src/components/Productos.jsx
@@ -19,13 +19,34 @@ function Productos() {
             </div>
             <div className="row row-macetas align-items-center">
             {pots.map((pot) => (
-            <div className="pot-card col-md-4 text-center mb-5" key={pot.id}>
+            <div className="pot-card col-md-4 text-center mb-5" key={pot.id}
+            onClick={() => openModal(pot)}>
                 <img src={pot.image} alt={pot.name} className="pot-image" />
                 <h2 className="pot-name">{pot.name}</h2>
                 <p className="pot-description">{pot.description}</p>
             </div>
             ))}
             </div>
+
+            {/* Modal */}
+            {isModalOpen && selectedPot && (
+                <div className="modal-overlay" onClick={closeModal}>
+                <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+                    <button className="modal-close" onClick={closeModal}>
+                    &times;
+                    </button>
+                    <img
+                    src={selectedPot.image}
+                    alt={selectedPot.name}
+                    className="modal-image"
+                    />
+                    <h2>{selectedPot.name}</h2>
+                    <p>{selectedPot.description}</p>
+                    <p className="modal-price">Precio: $ {selectedPot.price}</p>
+                </div>
+                </div>
+            )}
+
         </section>
     );
   }
